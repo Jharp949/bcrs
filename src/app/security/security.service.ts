bb94c1d774218c744d259d8fd3e1c889cff4ccf9
James Harper <jharp949@gmail.com>
2024-02-17 21:09:28 -0800

    Update import statement in signin.service.spec.ts

---
+++ src/app/security/security.service.ts
@@ -0,0 +1,20 @@
+/**
+ * Title: security.service.ts
+ * Author: James Harper
+ * Date: 1/31/24
+ */
+
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SecurityService {
+
+  constructor(private http: HttpClient) { }
+
+  findEmployeeById(empId: number) {
+    return this.http.get('/api/users/find-one' + empId); // retunrns the employee object
+  }
+}
