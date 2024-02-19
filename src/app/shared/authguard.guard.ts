61764f35ffaf26a7e465f52acfdd606d4a9e5f3d
James Harper <jharp949@gmail.com>
2024-02-18 21:27:34 -0800

    Refactor authGuard to use injected services***

--- src/app/shared/authguard.guard.ts
+++ src/app/shared/authguard.guard.ts
@@ -1,23 +1,26 @@
 /*
 * Project Name: authguard.guard.ts
-* Authors: Laurel Condon, James Harper, Danielle Taplin
+* Author: Professor Krasso
+* Contributors: Laurel Condon, James Harper, Danielle Taplin
 * Date: 2/12/2024
 */
 
-import { inject } from '@angular/core';
+// Import statements
 import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  const cookie = inject(CookieService); // injects the cookie service
+  const cookie = inject(CookieService); // Inject the cookie service from the ngx-cookie-service package
 
+  /**
+   * if the user is logged in, allow the route to load
+   */
   if (cookie.get('session_user')) {
-    console.log('User is logged in and can access the Utilities page');
-    return true;
+    return true; // Allow the route to load
   } else {
-    console.log('User is not logged in and cannot access the Utilities page');
-    const router = inject(Router); // injects the router
-    router.navigate(['/signin'], { queryParams:  {returnUrl: state.url }}); // navigates the user to the signin page
-    return false;
+    const router = inject(Router); // Inject the router service from the @angular/router package
+    router.navigate((['/signin'])); // Redirect to login page
+    return false; // Prevent the route from loading
   }
 };
